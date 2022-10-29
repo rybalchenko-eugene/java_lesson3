47ad1cda7e935cc59bff701ef852f7a18ae07aa5
rybalchenko-eugene <106405926+rybalchenko-eugene@users.noreply.github.com>
2022-10-29 13:20:39 +0300

    Knite passes all fields

--- lib.java
+++ lib.java
@@ -3,25 +3,40 @@ public class lib {
 
     public static int [][] new_board(){
         Scanner in = new Scanner(System.in);
-        System.out.print("Input a number of column: ");
+        System.out.print("Input a number of columns: ");
         int col = in.nextInt();
-        System.out.print("Input a number of column: ");
+        System.out.print("Input a number of rows: ");
         int row = in.nextInt();
         System.out.printf("Your board has measures: %d x %d \n", col, row);
         in.close();
         int [] [] board = new int [row+4][col+4];
-        for (int i = 0; i < row+4; i++) {
+        // Making margin
+        for (int i = 0; i < board[0].length; i++) {
             board[0][i] = -1;
             board[1][i] = -1;
-            board[col + 2][i] = -1;
-            board[col + 3][i] = -1;
+            board[row + 2][i] = -1;
+            board[row + 3][i] = -1;
+        }
+        for (int i = 0; i < board.length; i++) {
             board[i][0] = -1;
             board[i][1] = -1;
-            board[i][row + 2] = -1;
-            board[i][row + 3] = -1;
+            board[i][col + 2] = -1;
+            board[i][col + 3] = -1;
         }
         board[2][2] = 1;
         return board;
+    }
+    
+    public static int count_mov(int [][]b, int x, int y) {
+        int count = 0;
+        int [] mov1 = { -2, -1, 1, 2, 2, 1, -1, -2 };
+        int [] mov2 = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        for (int i = 0; i < mov1.length; i++) {
+            if (b[y + mov1[i]][x + mov2[i]] == 0) {
+                count++;
+            }
+        }
+        return count;
     }
 }
 
--- programm.java
+++ programm.java
@@ -5,7 +5,7 @@ public class programm {
 
     public static void main(String[] args) {
         int [][] lst = lib.new_board();              
-        
+        print2(lst);
         int [] path = new int [128];
         path[0] = 2;
         path[1] = 2;
@@ -18,15 +18,29 @@ public class programm {
     }
 
     public static void print2(int [][]lst) {
+        int flag = 1;
         for (int i = 0; i < lst.length; i++) {
-            for (int j = 0; j < lst[i].length; j++) {
+            flag = i%2 == 0? 1 : -1;
+
+            for (int j = 0; j < lst[0].length; j++) {
                 if (lst[i][j] == -1) {
+                    if (flag == 1) {                    
                     System.out.print("███");
+                    flag *=-1;
+                    }
+                    else {
+                        System.out.print("   ");
+                        flag *=-1;
+                    }
                 }
                 else if (lst[i][j] / 10 > 0) {
                     System.out.print(" " + lst[i][j]);
-                }
-                else System.out.print("  " + lst[i][j]);
+                    flag *=-1;
+                    }
+                    else {
+                        System.out.print("  " + lst[i][j]);
+                        flag *=-1;
+                    }
             }
             System.out.println();
         }
@@ -43,8 +57,8 @@ public class programm {
             int min = 8;
             for (int j = 0; j < mov1.length; j++) {
                 if (b[y + mov1[j]][x + mov2[j]] == 0) {
-                    if  (count_mov(b, x + mov2[j], y + mov1[j]) < min) {
-                        min = count_mov(b, x + mov2[j], y + mov1[j]);
+                    if  (lib.count_mov(b, x + mov2[j], y + mov1[j]) < min) {
+                        min = lib.count_mov(b, x + mov2[j], y + mov1[j]);
                         opt_move = j; // Pick the opt_move by min of variants
                     }            
                 }
@@ -63,31 +77,6 @@ public class programm {
         }
     }
 
-    public static int count_mov(int [][]b, int x, int y) {
-        int count = 0;
-        int [] mov1 = { -2, -1, 1, 2, 2, 1, -1, -2 };
-        int [] mov2 = { 1, 2, 2, 1, -1, -2, -2, -1 };
-        for (int i = 0; i < mov1.length; i++) {
-            if (b[y + mov1[i]][x + mov2[i]] == 0) {
-                count++;
-            }
-        }
-        return count;
-}
 
-    public static void add(int []lst, int x, int y) {
-        for (int i = 0; i < lst.length-2 ; i++) {
-            if (lst[i] == 0) {
-                lst[i] = x;
-                lst[i+1] = y;
-                break;
-            }
-        }
-    }
 
-    public static void remove(int []lst) {
-        for (int i = 0; i < lst.length-3; i++) {
-            lst[i] = lst[i+2];
-        }
-    }
 }
